986f43813cf8563f8a5de5f1538da8174ec56b01
Julian <julian.57@outlook.fr>
2023-06-25 18:14:10 +0200

    Del unused function

--- chess/main.py
+++ chess/main.py
@@ -101,8 +101,6 @@ class Main():
                     # start the game black (reverse the board)
                     # playerOne : False & playerTwo : True
                     self.main_game(playerOne=False, playerTwo=True)
-                    flip_board = game_state.flip_board(game_state.board)
-                    print(flip_board)
 
             if white_button.collidepoint(mouse_pos):
                 if p.mouse.get_pressed()[0]:
