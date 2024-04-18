906ed7162026b810ef16b73c622c74d83ecbd71b
Initium <97995554+nixiz0@users.noreply.github.com>
2024-04-18 09:40:37 +0200

    Add activate env on start-app.bat

--- start-app.bat
+++ start-app.bat
@@ -2,10 +2,12 @@
 IF NOT EXIST .env (
     python -m venv .env
     cd .env\Scripts
+    call activate.bat
     cd ../..
     pip install -r requirements.txt
 ) ELSE (
     cd .env\Scripts
+    call activate.bat
     cd ../..
 )
 python.exe menu.py
